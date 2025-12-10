4c9934b76c180b0d52e3c35adc89777e70e2039c
jonathan <jonathanfiorenza6@gmail.com>
2025-12-09 22:24:16 -0300

    finalizamos con las funciones para q nos de un  numero aletorio entre dos argumentos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log("Hola Mundo desde Node.js");
+/*
+nombre de ramas
+feature/funciones -- es para nuevas funcionalidades
+fix/nombre rama --es para un arreglo de la rama
+realese/ nombre rama-- es para se acomoda los codigos antes de pasar a produccion*/
