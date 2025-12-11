22f971a80ee47106f4ccf76f731399a8df67d13b
jonathan <jonathanfiorenza6@gmail.com>
2025-12-11 10:52:42 -0300

    agregamos el modulo de colores en la terminal de leer archivos

--- src/4-leerArchivos.js
+++ src/4-leerArchivos.js
@@ -1,13 +1,15 @@
 //ejemplos de modulos fs
 
 const fs = require('fs');
+const colors = require('colors')
+
 
 try {
 const data = fs.readFileSync('./canciones.txt', 'utf8'); //readFileSync es sincrono sirve para leer archivos de texto
 console.log('Contenido del archivo:', data);
 
 } catch (error) {
-console.error('Error al leer el archivo:', error);
+ console.error(`${'ocurrio un error al leer el archivo:'.bgRed.white} \n ${error}`)
     
 
 }
--- src/6-colors.js
+++ src/6-colors.js
@@ -1,3 +1,6 @@
-const colors = (`colors`)
+const colors = require('colors')
 
-console.error(`ocurrio un error`.bgRed.white)
+console.error('Ocurrio un error'.bgRed.white)
+console.log('Agregamos canciones'.green)
+console.info('Ejemplo de texto subrayado'.underline.cyan)
+console.log('Ejemplo de texto 2'.rainbow)
